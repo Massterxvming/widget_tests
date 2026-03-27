e09ac36284650109884ef107a1007434f39e399c
Massterxvming <2260947414@qq.com>
2026-03-27 21:22:48 +0800

    11

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
   // await ObjectData.instance.init();
   await MMKVData.init();
   await AppController.instance.init();
-  await Get.putAsync(AppController.instance.init() as AsyncInstanceBuilderCallback);
+  //韩磊傻逼
 
   runApp(const MyApp());
 }
